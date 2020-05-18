a574107fd15fecc847762ba55fd14cb61b836b15
Dung Nguyen Van <48980337+dungxd98@users.noreply.github.com>
2020-05-18 23:59:10 +0700

    update database add address (AuthenticationContext)

--- Migrations/20200518165651_UpdateTableUser_Address.Designer.cs
+++ Migrations/20200518165651_UpdateTableUser_Address.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoesStoreApi.Models;
 
 namespace ShoesStoreApi.Migrations
 {
     [DbContext(typeof(AuthenticationContext))]
-    partial class AuthenticationContextModelSnapshot : ModelSnapshot
+    [Migration("20200518165651_UpdateTableUser_Address")]
+    partial class UpdateTableUser_Address
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Migrations/20200518165651_UpdateTableUser_Address.cs
+++ Migrations/20200518165651_UpdateTableUser_Address.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ShoesStoreApi.Migrations.ShoesStoreApi
+namespace ShoesStoreApi.Migrations
 {
     public partial class UpdateTableUser_Address : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
         }
     }
 }
