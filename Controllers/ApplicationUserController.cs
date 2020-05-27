edcb3feaf0bafd9fd9df46070d1b2015a6084c5f
trunghai029 <55788174+trunghai029@users.noreply.github.com>
2020-05-27 15:56:10 +0700

    request active to login

--- Controllers/ApplicationUserController.cs
+++ Controllers/ApplicationUserController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -49,25 +49,38 @@ namespace ShoesStoreApi.Controllers {
         //POST : api/AplicationUser/Login
         public async Task<IActionResult> Login (LoginModel model) {
             var user = await _userManager.FindByNameAsync (model.UserName);
-            if (user != null && await _userManager.CheckPasswordAsync (user, model.Password)) {
-                //Get role assigned to the user
-                var role = await _userManager.GetRolesAsync(user);
-                IdentityOptions _options = new IdentityOptions();
 
-                var tokenDescriptor = new SecurityTokenDescriptor {
-                Subject = new ClaimsIdentity (new Claim[] {
+            if(user.Status == "Active")
+            {
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    //Get role assigned to the user
+                    var role = await _userManager.GetRolesAsync(user);
+                    IdentityOptions _options = new IdentityOptions();
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new Claim[] {
                 new Claim ("UserID", user.Id.ToString ()),
                 new Claim(_options.ClaimsIdentity.RoleClaimType,role.FirstOrDefault())
                 }),
-                Expires = DateTime.UtcNow.AddDays (1),
-                SigningCredentials = new SigningCredentials (new SymmetricSecurityKey (Encoding.UTF8.GetBytes (_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
-                };
-                var tokenHandler = new JwtSecurityTokenHandler ();
-                var securityToken = tokenHandler.CreateToken (tokenDescriptor);
-                var token = tokenHandler.WriteToken (securityToken);
-                return Ok (new { token });
-            } else
-                return BadRequest (new { message = "Username or password is incorrect." });
+                        Expires = DateTime.UtcNow.AddDays(1),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                    var token = tokenHandler.WriteToken(securityToken);
+                    return Ok(new { token });
+                }
+                else
+                {
+                    return BadRequest(new { message = "Username or password is incorrect." });
+                }   
+            }
+            else 
+            {
+                return StatusCode(403);
+            }
         }
     }
 }
